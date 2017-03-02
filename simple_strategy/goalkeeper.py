43b1d8db29f120118b3c10ee80963c376285f86b
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-03-02 14:41:24 -0300

    Improved goalkeeper script
    Still needs testing

--- simple_strategy/goalkeeper.py
+++ simple_strategy/goalkeeper.py
@@ -5,19 +5,38 @@ from unball.msg import MeasurementSystemMessage
 from communication.msg import target_positions_msg
 from subprocess import call
 
-def callback(data):
-    msg = target_positions_msg()
-    target = [data.ball_x, data.ball_y]
-    # msg.x[0] = target[0]
-    msg.y[0] = target[1]
-    pub.publish(msg)
+our_field_side = 'left'
 
-def start():
+def main():
     global pub
+    rospy.init_node('goalkeeper')
     pub = rospy.Publisher('target_positions_topic', target_positions_msg, queue_size=10)
-    rospy.init_node('strategy')
     rospy.Subscriber('measurement_system_topic', MeasurementSystemMessage, callback)
     rospy.spin()
 
+def callback(data):
+    msg = target_positions_msg()
+    goalkeeperObjective = calculateGoalkeeperObjective([data.ball_x, data.ball_y])
+    msg.x[0] = goalkeeperObjective[0]
+    msg.y[0] = goalkeeperObjective[1]
+
+    # Make other robots stay in position
+    for i in [1, 2]:
+        msg.x[i] = data.x[i]
+        msg.y[i] = data.y[i]
+    pub.publish(msg)
+
+def calculateGoalkeeperObjective(ball_pos):
+    objective = [0, 0]
+    objective[0] = 0.65
+    if our_field_side == 'left':
+        objective[1] = ball_pos[1]
+    if objective[1] > 0.13:
+        objective[1] = 0.13
+    if objective[1] < -0.13:
+        objective[1] = -0.13
+    return objective
+
+
 if __name__ == '__main__':
-    start()
+    main()
