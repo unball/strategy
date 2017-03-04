b84353987baab13130fab7b3be93243001f028cd
vitorduarte <vitor.alves.duarte@gmail.com>
2017-03-04 02:19:37 -0300

    Added a test for tangential potential field.

---
+++ simple_strategy/test_tangential_potential.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+
+import potential_fields as pf
+from unball.msg import MeasurementSystemMessage
+from communication.msg import target_positions_msg
+from subprocess import call
+
+# This script is made to test the tangential potential around the ball.
+# If you need to change the behavior of this potential field you can change this constant:
+#   * TANGENCIAL_BALL_MAGNITUDE - Indicates the magnitude of field
+#
+# You can see the potential_fields.py to more informations
+
+
+TANGENCIAL_BALL_MAGNITUDE = 1.5
+
+number_of_robots = 3
+
+def callback(data):
+    msg = target_positions_msg()
+
+    #PCreating an tangential field
+    tangencial_ball = pf.TangencialPotentialField(
+                        np.array([data.ball_x, data.ball_y]),
+                        TANGENCIAL_BALL_MAGNITUDE)
+
+    for robot in range(number_of_robots):
+
+        resultant_vector = tangencial_ball.calculate_force(
+                                np.array([data.x[robot], data.y[robot]]))
+        msg.x[robot] = resultant_vector[0]
+        msg.y[robot] = resultant_vector[1]
+
+    pub.publish(msg)
+
+
+def start():
+    global pub
+    pub = rospy.Publisher('target_positions_topic', target_positions_msg, queue_size=10)
+    rospy.init_node('strategy')
+    rospy.Subscriber('measurement_system_topic', MeasurementSystemMessage, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    start()
