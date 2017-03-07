72050cd19edf912082c8b56d6688ba69bbcc35a2
vitorduarte <vitor.alves.duarte@gmail.com>
2017-03-07 01:07:01 -0300

    correct kicker player

--- simple_strategy/kicker.py
+++ simple_strategy/kicker.py
@@ -15,41 +15,43 @@ allies = [ [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
 class Kicker(Player):
     def getTarget(self):
-        K_ball = 20.0
+        K_ball = 10
 
         robot = np.array([self.allies[self.my_index][0], self.allies[self.my_index][1]])
 
-        side_opponent = self.field_side
-        if(side_opponent == 'Right'):
-            opponent_goal =  np.array([0.8, 0])
-        else:
-            opponent_goal =  np.array([-0.8, 0])
+        opponent_goal = self.get_opponent_goal()
 
         goal_to_ball = opponent_goal - self.ball
-        goal_to_ball[1] = -goal_to_ball[1]
-        goal_to_ball_angle = pf.cart2polar(goal_to_ball)[1]
         goal_to_ball_mag = pf.cart2polar(goal_to_ball)[0]
+        goal_to_ball_angle = pf.cart2polar(goal_to_ball)[1]
 
         goal_to_robot = opponent_goal - robot
-        goal_to_robot_angle = pf.cart2polar(goal_to_robot)[1]
         goal_to_robot_mag = pf.cart2polar(goal_to_robot)[0]
+        goal_to_robot_angle = pf.cart2polar(goal_to_robot)[1]
 
-        robot_to_ball = robot - self.ball
+        robot_to_ball = self.ball - robot
         robot_to_ball_mag = pf.cart2polar(robot_to_ball)[0]
 
-        if((np.fabs(goal_to_ball_angle - goal_to_robot_angle) <= np.pi/15)
-            and (robot_to_ball_mag <= 0.1)
-            and is_on_attack_mode(robot[0], self.ball[0])):
-        #):
+        if((np.fabs(goal_to_ball_angle - goal_to_robot_angle) <= np.pi/10)
+                    and is_on_attack_mode(robot[0], self.ball[0])
+                    and (robot_to_ball_mag <= 0.1)):
             target = opponent_goal
-            target[0] = -target[0]
             print ' gol de placa'
         else:
-            target = (self.ball + goal_to_robot/K_ball)
-            print ' preparando'
+            target = (self.ball - goal_to_ball/K_ball)
+            #target[1] += 0.3
+            print 'preparando'
 
         return target
 
+    def get_opponent_goal(self):
+        global side_opponent
+        side_opponent = 'Left' if self.field_side == 'Right' else 'Right'
+        print side_opponent
+        if(side_opponent == 'Right'):
+            return np.array([0.8, 0])
+        else:
+            return np.array([-0.8, 0])
 player = Kicker()
 
 def callback(data):
@@ -71,9 +73,9 @@ def callback(data):
 
 def is_on_attack_mode(x_robot, x_ball):
     if(side_opponent == 'Right'):
-       return x_robot > x_ball
+       return x_robot < x_ball
     else:
-        return x_robot < x_ball
+        return x_robot > x_ball
 
 def start():
     global pub
