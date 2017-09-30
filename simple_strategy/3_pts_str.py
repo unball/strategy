37157d0da61a4c1a7c8067686ae0608f447fd303
Igor Beduin <igor.rob.96@gmail.com>
2017-09-30 16:53:54 -0300

    New strategy that sets new targets in a 5s interval

---
+++ simple_strategy/3_pts_str.py
@@ -0,0 +1,38 @@
+import rospy
+import math
+from measurement_system.msg import measurement_msg
+from communication.msg import target_positions_msg
+
+number_of_robots = 3
+
+global start_time
+
+global position
+
+def set_3_pnts(msg):
+    current_time = int(rospy.get_time()) - int(start_time)
+
+    for robot in range(number_of_robots):
+        msg.x[robot] = position
+        msg.y[robot] = position
+
+
+def callback(data):
+    msg = target_positions_msg()
+    set_3_pnts(msg)
+    pub.publish(msg)
+    rospy.sleep(10.)
+    position = -1 * position
+
+
+def start():
+
+    global pub
+    pub = rospy.Publisher('target_positions_topic', target_positions_msg, queue_size=10)
+    rospy.Subscriber('measurement_system_topic', measurement_msg, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('strategy')
+    start_time = rospy.get_time()
+    start()
