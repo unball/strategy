de6065c173131d76a759f89177416b67c23b499f
sousasmurf <g.sousasmurf@gmail.com>
2017-09-30 14:58:33 -0300

    circle strategy its working

--- simple_strategy/circle.py
+++ simple_strategy/circle.py
@@ -1,16 +1,16 @@
 import rospy
 import math
-from player import *
 from measurement_system.msg import measurement_msg
 from communication.msg import target_positions_msg
-from subprocess import call
 
 number_of_robots = 3
 #Class Circle
-w = 0.1
-r = 1
+w = 1
+r = 0.2
 org_x = 0
 org_y = 0
+
+global start_time
 
 def callback(data):
     msg = target_positions_msg()
@@ -21,26 +21,29 @@ def obt_circle(msg):
     current_time = rospy.get_time() - start_time
 
     th = w * current_time
-    xd = (r * cos(th)) + org_x
-    yd = (r * sin(th)) + org_y
+    xd = (r * math.cos(th))
+    yd = (r * math.sin(th))
 
     for robot in range(number_of_robots):
         if robot == 0:
             msg.x[robot] = xd
             msg.y[robot] = yd
+        elif robot == 1:
+            msg.x[robot] = 0.5
+            msg.y[robot] = 0.5
         else:
-            msg.x[robot] = org_x
-            msg.y[robot] = org_y
+            msg.x[robot] = -0.5
+            msg.y[robot] = 0.5
 
 def start():
 
     global pub
     pub = rospy.Publisher('target_positions_topic', target_positions_msg, queue_size=10)
-    rospy.init_node('strategy')
-    start_time = rospy.get_time()
     rospy.Subscriber('measurement_system_topic', measurement_msg, callback)
     rospy.spin()
 
 
 if __name__ == '__main__':
+    rospy.init_node('strategy')
+    start_time = rospy.get_time()
     start()
