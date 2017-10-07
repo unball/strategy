1063d7473db5f0bf8f673cf8c9f8bdc561ce99e7
sousasmurf <g.sousasmurf@gmail.com>
2017-10-07 10:31:23 -0300

    3_pts_position added

--- strategy_general/3_pts_position.py
+++ strategy_general/3_pts_position.py
@@ -7,22 +7,30 @@ from strategy.msg import strategy_output_msg
 from control_options import *
 
 
+
+
 def callback(data):
-    msg = target_positions_msg()
-    msg2 = strategy_output_msg()
-    msg.x[0]=2
-    msg2.x[0]=3
-    
-    msg2.control_options[0]=control_options.position
+    msg = strategy_output_msg()
+
+    msg.control_options = [control_options.position, control_options.position, control_options.position]
+
+    msg.x[0] = -0.5
+    msg.y[0] = 0.5
+
+    msg.x[1] = 0
+    msg.y[1] = 0.5
+
+    msg.x[2] = 0.5
+    msg.y[2] = 0.5
 
     pub.publish(msg)
-    pub2.publish(msg2)
+    
+
+
 
 def start():
     global pub
-    global pub2
-    pub = rospy.Publisher('target_positions_topic', target_positions_msg, queue_size=10)
-    pub2 = rospy.Publisher('strategy_output_topic', strategy_output_msg, queue_size=10)
+    pub = rospy.Publisher('strategy_output_topic', strategy_output_msg, queue_size=10)
     rospy.init_node('strategy')
     rospy.Subscriber('measurement_system_topic', measurement_msg, callback)
     rospy.spin()
