48398e8e9a7b2527a2c0cd42a04897fc6f21de28
imota <mota.icaro@gmail.com>
2017-11-05 15:56:42 -0500

    commited last icaro's efforts

---
+++ new_arch/abstract_strategy.py
@@ -0,0 +1,24 @@
+import math
+from point import Point
+from control_options import *
+from field_side import *
+
+class AbstractStrategy():
+
+    def set_robot_state(self, robot_point, robot_th, robot_id):
+        self.position = robot_point
+        self.th = robot_th
+        self.id = robot_id
+
+    def set_ball_position(self, ball_pos):
+        self.ball_pos = ball_pos
+
+    def enemy_state(self, enemy_pos):
+        pass
+
+    def stop(self):
+        return [self.position.x, self.position.y, self.th, 0, 0, 0, 0, control_options.direct_speeds]
+
+    @property
+    def fieldSide(self):
+        return FieldSide.side
---
+++ new_arch/field_side.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Side(Enum):
+	RIGHT = "Right"
+	LEFT = "Left"
+	
+class FieldSide():
+	side = Side.RIGHT
+
