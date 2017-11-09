eb48ed4051d13d431322e645f003a38eb0684fa4
Vieira Neto <vieiranetoc@gmail.com>
2017-11-09 05:13:33 -0200

    GoToBall doesn't go beyond a limit x value

--- new_arch/behaviours/go_to_ball.py
+++ new_arch/behaviours/go_to_ball.py
@@ -7,6 +7,7 @@ class GoToBall(AbstractStrategy):
         self.th = 0
         self.tolerance_radius = 0.1
         self.goal = Point(0, 0)
+        self.lim_x = 0.50
 
     def get_strategy_output(self):
         return [self.goal.X, self.goal.Y, 0, self.u, 0, 0, 0, self.control_option]
@@ -15,15 +16,26 @@ class GoToBall(AbstractStrategy):
         if math.sqrt((self.ball_pos.X - self.position.X)**2 + (self.ball_pos.Y - self.position.Y)**2) <= self.tolerance_radius:
             if self.fieldSide == Side.RIGHT:
                 if self.ball_pos.Y > 0:
-                    self.u = 4
-                else:
                     self.u = 3
+                else:
+                    self.u = 4
 
             if self.fieldSide == Side.LEFT:
                 if self.ball_pos.Y > 0:
-                    self.u = 3
-                else:
                     self.u = 4
+                else:
+                    self.u = 3
         else:
-            self.goal = Point(self.ball_pos.x, self.ball_pos.y)
+            if self.fieldSide == Side.RIGHT:
+                if self.ball_pos.X >= self.lim_x:
+                    self.goal = Point(self.lim_x, self.ball_pos.y)
+                else:
+                    self.goal = Point(self.ball_pos.x, self.ball_pos.y)
+
+            if self.fieldSide == Side.LEFT:
+                if self.ball_pos.X <= self.lim_x:
+                    self.goal = Point(-self.lim_x, self.ball_pos.y)
+                else:
+                    self.goal = Point(self.ball_pos.x, self.ball_pos.y)                
+
             self.u = 0
